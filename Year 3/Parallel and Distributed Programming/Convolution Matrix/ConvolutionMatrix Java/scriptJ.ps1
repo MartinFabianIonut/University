e8c59f87a528f68d80ea22d57004537f49e9ea4a
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-10-26 17:35:30 +0300

    Scripts added for Java

--- Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix Java/scriptJ.ps1
+++ Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix Java/scriptJ.ps1
@@ -8,35 +8,50 @@ $param5 = $args[5] # Convolution matrix
 $suma = 0
 $difference = 0
 
-for ($i = 0; $i -lt $param3; $i++){
-    Write-Host "Rulare" ($i+1)
-    $a = java $param0 $param2 $param4 $param5
-    Write-Host $a
-    $suma += $a
-    Write-Host ""
-    $fileA = "$PSScriptRoot\Outputs\sequential.txt"
-    $fileB = "$PSScriptRoot\Outputs\parallel.txt"
-    if(Compare-Object -ReferenceObject $(Get-Content $fileA) -DifferenceObject $(Get-Content $fileB)) {
-        "files are different"
-        $difference += 1
-    }
-    Else {
-        "files are the same"
-    }
-
-}
-$media = $suma / $i
-
-Write-Host "Timp de executie mediu:" $media
-
 if (!(Test-Path outJ.csv)){
     New-Item outJ.csv -ItemType File
     Set-Content outJ.csv 'Tip Matrice,Nr threads,Timp executie'
 }
 
+for ($i = 0; $i -lt 15*$param3; $i++){
+
+    if($i % $param3 -eq 0 -and $i -ne 0)
+    {
+        $media = $suma / $param3
+        if ($difference -eq 0){
+            Add-Content outJ.csv "$param1,$param2,$media"
+        }
+        else{
+            Add-Content outJ.csv "$param1,$param2,Diferente"
+            Write-Host "Diferente"
+        }
+        Write-Host "Timp de executie mediu:" $media
+        Write-Host ""
+        $suma = 0
+    }
+
+    # Write-Host "Rulare" ($i+1)
+    $a = java $param0 $param2 $param4 $param5
+    # Write-Host $a
+    $suma += $a
+    # Write-Host ""
+    $fileA = "$PSScriptRoot\Outputs\sequential.txt"
+    $fileB = "$PSScriptRoot\Outputs\parallel.txt"
+    if(Compare-Object -ReferenceObject $(Get-Content $fileA) -DifferenceObject $(Get-Content $fileB)) {
+        # "files are different"
+        $difference += 1
+    }
+    Else {
+        # "files are the same"
+    }
+
+}
+$media = $suma / $param3
 if ($difference -eq 0){
     Add-Content outJ.csv "$param1,$param2,$media"
 }
 else{
     Add-Content outJ.csv "$param1,$param2,Diferente"
+    Write-Host "Diferente"
 }
+Write-Host "Timp de executie mediu:" $media
--- Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix Java/scriptJSequential.ps1
+++ Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix Java/scriptJSequential.ps1
@@ -1,26 +1,35 @@
 $param0 = $args[0] # Class name
 $param1 = $args[1] # Matrix type
-$param3 = $args[3] # No of runs
-$param4 = $args[4] # Input matrix
-$param5 = $args[5] # Convolution matrix
+$param2 = $args[2] # No of runs
+$param3 = $args[3] # Input matrix
+$param4 = $args[4] # Convolution matrix
 
 $suma = 0
-
-for ($i = 0; $i -lt $param3; $i++){
-    Write-Host "Rulare" ($i+1)
-    $a = java $param0 $param2 $param4 $param5
-    Write-Host $a
-    $suma += $a
-    Write-Host ""
-
-}
-$media = $suma / $i
-
-Write-Host "Timp de executie mediu:" $media
 
 if (!(Test-Path outJ.csv)){
     New-Item outJ.csv -ItemType File
     Set-Content outJ.csv 'Tip Matrice,Nr threads,Timp executie'
 }
 
+for ($i = 0; $i -lt 5*$param2; $i++){
+    if($i % $param2 -eq 0 -and $i -ne 0)
+    {
+        $media = $suma / $param2
+        Write-Host "Timp de executie mediu:" $media
+        Write-Host ""
+        Add-Content outJ.csv "$param1,Sequential,$media"
+        $suma = 0
+    }
+
+    # Write-Host "Rulare" ($i+1)
+    $a = java $param0 $param3 $param4
+    # Write-Host $a
+    $suma += $a
+    # Write-Host ""
+
+}
+$media = $suma / $param2
+
+Write-Host "Timp de executie mediu:" $media
+Write-Host ""
 Add-Content outJ.csv "$param1,Sequential,$media"
