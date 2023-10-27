5c4024544e5c05d3922bbced934c28ef4998e868
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-10-27 11:03:29 +0300

    Updated in BookStore

--- Year 3/Programming for Mobile Devices/BookStore/src/pages/BookEdit.tsx
+++ Year 3/Programming for Mobile Devices/BookStore/src/pages/BookEdit.tsx
@@ -5,13 +5,12 @@ import {
     IonContent,
     IonHeader,
     IonInput,
+    IonLabel,
     IonLoading,
     IonPage,
     IonTitle,
     IonToolbar,
-    IonLabel,
     IonCheckbox,
-    IonDatetimeButton
 } from '@ionic/react';
 import { getLogger } from '../core';
 import { BookContext } from './BookProvider';
@@ -19,11 +18,11 @@ import { RouteComponentProps } from 'react-router';
 import { BookProps } from './BookProps';
 import { format } from 'date-fns';
 import moment from 'moment';
+import { render } from '@testing-library/react';
+
 const log = getLogger('BookEdit');
 
-interface BookEditProps extends RouteComponentProps<{
-    id?: string;
-}> { }
+interface BookEditProps extends RouteComponentProps<{ id?: string }> { }
 
 const styles = {
     page: {
@@ -36,40 +35,94 @@ const styles = {
     content: {
         padding: '20px',
     },
-    input: {
-        color: '#be1',
-        borderRadius: '5px',
+    inputContainer: {
         marginBottom: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        boxShadow: '0 4px 8px rgba(22, 14, 143, 0.507)',
+    },
+    label: {
+        marginLeft: '20px',
+        marginRight: '10px',
+        minWidth: '120px',
+        color: 'var(--ion-color-tertiary)',
+        fontWeight: 'bold'
+    },
+    input: {
+        flex: 1,
+        borderRadius: '5px',
+        color: 'var(--ion-color-tertiary-tint)',
     },
     checkbox: {
-        marginTop: '10px',
+        marginTop: '20px',
+        marginBottom: '20px'
     },
     button: {
         backgroundColor: '#4caf50',
         color: '#fff',
+        borderRadius: '15px',
     },
     errorMessage: {
         color: '#ff0000',
         marginTop: '10px',
     },
+    title: {
+        fontFamily: 'Geneva, sans-serif',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+    }
 };
+
+function parseDDMMYYYY(dateString: string) {
+    const [day, month, year] = dateString.split('/').map(Number);
+
+    // Validate the components
+    if (isNaN(day) || isNaN(month) || isNaN(year)) {
+        log('Invalid date components');
+        return null;
+    }
+
+    // Months are zero-based, so subtract 1 from the month
+    const adjustedMonth = month - 1;
+
+    // Create the Date object
+    const parsedDate = new Date(year, adjustedMonth, day);
+
+    if (
+        parsedDate.getDate() !== day ||
+        parsedDate.getMonth() !== adjustedMonth ||
+        parsedDate.getFullYear() !== year
+    ) {
+        console.error('Invalid date');
+        return null;
+    }
+
+    // Validate the Date object
+    if (isNaN(parsedDate.getTime())) {
+        log('Invalid date');
+        return null;
+    }
+
+    return parsedDate;
+}
 
 const BookEdit: React.FC<BookEditProps> = ({ history, match }) => {
     const { books, saving, savingError, saveBook: saveBook } = useContext(BookContext);
     const [book, setBook] = useState<BookProps | undefined>(undefined);
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('Type a title');
+    const [author, setAuthor] = useState('Type an author');
     const [publicationDate, setPublicationDate] = useState<Date | undefined>(undefined);
     const [isAvailable, setIsAvailable] = useState(false);
     const [price, setPrice] = useState(0);
 
     useEffect(() => {
-        log('useEffect');
+        log('useEffect - Fetching book details');
         const routeId = match.params.id || '';
-        const foundBook = books?.find(book => book.id === routeId);
+        const foundBook = books?.find((book) => book.id === routeId);
         setBook(foundBook);
 
         if (foundBook) {
+            log('useEffect - Setting book details');
             setTitle(foundBook.title);
             setAuthor(foundBook.author);
             setPublicationDate(foundBook.publicationDate);
@@ -85,51 +138,71 @@ const BookEdit: React.FC<BookEditProps> = ({ history, match }) => {
             author,
             publicationDate: publicationDate || new Date(),
             isAvailable,
-            price
+            price,
         };
 
-        saveBook && saveBook(editedBook).then(() => history.goBack());
+        log('handleSave - Saving edited book');
+        saveBook && saveBook(editedBook).then(() => {
+            log('handleSave - Book saved successfully. Navigating back.');
+            history.goBack();
+        });
     }, [book, saveBook, title, author, publicationDate, isAvailable, price, history]);
 
-    log('render');
+    log('render ' + title);
     return (
-        <IonPage>
-            <IonHeader class='header'>
+        <IonPage style={styles.page} >
+            <IonHeader style={styles.header}>
                 <IonToolbar>
-                    <IonTitle>Edit</IonTitle>
+                    <IonTitle style={styles.title} color="primary">Edit</IonTitle>
                     <IonButtons slot="end">
-                        <IonButton onClick={handleSave}>Save</IonButton>
+                        <IonButton style={styles.button} onClick={handleSave}>
+                            Save
+                        </IonButton>
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
-            <IonContent>
-                <div style={{ display: 'flex', alignItems: "center" }}>
-                    <IonLabel style={{ marginRight: '10px' }}>Title:</IonLabel>
-                    <IonInput style={styles.input} value={title} onIonChange={e => setTitle(e.detail.value || '')} />
+            <IonContent style={styles.content}>
+                <div style={styles.inputContainer}>
+                    <IonLabel style={styles.label}>Title:</IonLabel>
+                    <IonInput style={styles.input} value={title} onIonChange={(e) => setTitle(e.detail.value || '')} />
                 </div>
 
-                <IonLabel>Author:</IonLabel>
-                <IonInput style={styles.input} value={author} onIonChange={e => setAuthor(e.detail.value || '')} />
+                <div style={styles.inputContainer}>
+                    <IonLabel style={styles.label}>Author:</IonLabel>
+                    <IonInput style={styles.input} value={author} onIonChange={(e) => setAuthor(e.detail.value || '')} />
+                </div>
 
-                <IonLabel>Publication Date:</IonLabel>
-                <IonInput class="input"
-                    value={publicationDate ? format(new Date(publicationDate), 'dd/MM/yyyy') : ''}
-                    onIonChange={e => {
-                        const inputDate = moment(e.detail.value, 'dd/MM/yyyy').toDate();
-                        setPublicationDate(inputDate || '');
-                    }}
-                />
+                <div style={styles.inputContainer}>
+                    <IonLabel style={styles.label}>Publication Date:</IonLabel>
+                    <IonInput
+                        class="input"
+                        style={styles.input}
+                        value={publicationDate ? format(new Date(publicationDate), 'dd/MM/yyyy') : ''}
+                        onIonChange={(e) => {
+                            const inputDate = parseDDMMYYYY(e.detail.value || '');
+                            if (inputDate !== null) {
+                                setPublicationDate(inputDate);
+                            }
+                            else {
+                                // If the date is invalid, change the input value to the previous valid date
+                                e.detail.value = publicationDate ? format(new Date(publicationDate), 'dd/MM/yyyy') : '';
+                            }
+                        }}
+                    />
+                </div>
 
-                <IonLabel>Available:</IonLabel>
-                <IonCheckbox class="checkbox" checked={isAvailable} onIonChange={e => setIsAvailable(e.detail.checked)} />
-                <br />
-                <IonLabel>Price:</IonLabel>
-                <IonInput class="input" value={price.toString()} onIonChange={(e) => setPrice(parseInt(e.detail.value || '0'))} />
+                <div style={styles.inputContainer}>
+                    <IonLabel style={styles.label}>Available:</IonLabel>
+                    <IonCheckbox style={styles.checkbox} checked={isAvailable} onIonChange={(e) => setIsAvailable(e.detail.checked)} />
+                </div>
+
+                <div style={styles.inputContainer}>
+                    <IonLabel style={styles.label}>Price:</IonLabel>
+                    <IonInput style={styles.input} value={price.toString()} onIonChange={(e) => setPrice(parseInt(e.detail.value || '0'))} />
+                </div>
 
                 <IonLoading isOpen={saving} />
-                {savingError && (
-                    <div>{savingError.message || 'Failed to save book'}</div>
-                )}
+                {savingError && <div style={styles.errorMessage}>{savingError.message || 'Failed to save book'}</div>}
             </IonContent>
         </IonPage>
     );
--- Year 3/Programming for Mobile Devices/BookStore/src/pages/BookList.tsx
+++ Year 3/Programming for Mobile Devices/BookStore/src/pages/BookList.tsx
@@ -2,8 +2,6 @@ import React, { useContext } from 'react';
 import { RouteComponentProps } from 'react-router';
 import {
     IonContent,
-    IonDatetime,
-    IonDatetimeButton,
     IonFab,
     IonFabButton,
     IonHeader,
@@ -15,22 +13,30 @@ import {
     IonToolbar,
 } from '@ionic/react';
 import { add } from 'ionicons/icons';
-import Book from './Book'; // Am schimbat denumirea la import
+import Book from './Book';
 import { getLogger } from '../core';
 import { BookContext } from './BookProvider';
 
-const log = getLogger('BookList'); // Am schimbat denumirea logului
+const log = getLogger('BookList');
+
+const styles = {
+    title: {
+        fontFamily: 'Geneva, sans-serif',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+    }
+};
 
 const BookList: React.FC<RouteComponentProps> = ({ history }) => {
-    const { books, fetching, fetchingError } = useContext(BookContext); // Am schimbat la BookContext
+    const { books, fetching, fetchingError } = useContext(BookContext);
 
-    log('render');
+    log('render ' + JSON.stringify(books));
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Book List</IonTitle>
+                    <IonTitle style={styles.title}>Book List</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
@@ -51,9 +57,7 @@ const BookList: React.FC<RouteComponentProps> = ({ history }) => {
                         ))}
                     </IonList>
                 )}
-                {fetchingError && (
-                    <div>{fetchingError.message || 'Failed to fetch books'}</div>
-                )}
+                {fetchingError && <div>{fetchingError.message || 'Failed to fetch books'}</div>}
                 <IonFab vertical="bottom" horizontal="end" slot="fixed">
                     <IonFabButton onClick={() => history.push('/book')}>
                         <IonIcon icon={add} />
