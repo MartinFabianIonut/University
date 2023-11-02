0b65e8c23ff3724cd9d99b30728add469bb80d0d
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-11-02 21:45:28 +0200

    Rename folders

--- Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix C++/ConvolutionMatrix C++ Parallel/Header Files/Paths/ConvolutionPaths.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-class ConvolutionPaths {
-public:
-	static const std::string paths[2];
-};
--- Year 3/Parallel and Distributed Programming/Convolution Matrix/ConvolutionMatrix C++/ConvolutionMatrix C++ Parallel/Source Files/Paths/ConvolutionPaths.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../Header Files/Paths/ConvolutionPaths.h"
-
-const std::string ConvolutionPaths::paths[2] = {
-	"C:\\GIT\\University\\Year 3\\Parallel and Distributed Programming\\ConvolutionMatrix C++\\ConvolutionMatrix C++ Parallel\\Resource Files\\Inputs\\convolution3x3.txt",
-	"C:\\GIT\\University\\Year 3\\Parallel and Distributed Programming\\ConvolutionMatrix C++\\ConvolutionMatrix C++ Parallel\\convolution5x5.txt"
-};
