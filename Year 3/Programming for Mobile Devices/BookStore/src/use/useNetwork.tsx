35d0f63edcb7b279eef275bededf9e81d2f88b09
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-11-06 23:54:59 +0200

    Update useNetwork.tsx
    We store a shouldSync string that commands synchronizing process

--- Year 3/Programming for Mobile Devices/BookStore/src/use/useNetwork.tsx
+++ Year 3/Programming for Mobile Devices/BookStore/src/use/useNetwork.tsx
@@ -1,37 +1,90 @@
+// import { useEffect, useState } from 'react';
+// import { Network, ConnectionStatus } from '@capacitor/network';
+// import { PluginListenerHandle } from '@capacitor/core';
+// import { getLogger } from '../core';
+
+// const initialState = {
+//   connected: false,
+//   connectionType: 'unknown',
+// }
+
+// const log = getLogger('useNetwork:');
+
+// export const useNetwork = () => {
+//   const [networkStatus, setNetworkStatus] = useState(initialState)
+//   useEffect(() => {
+//     let handler: PluginListenerHandle;
+//     registerNetworkStatusChange();
+//     Network.getStatus().then(handleNetworkStatusChange);
+//     let canceled = false;
+//     return () => {
+//       canceled = true;
+//       handler?.remove();
+//     }
+
+//     async function registerNetworkStatusChange() {
+//       handler = await Network.addListener('networkStatusChange', handleNetworkStatusChange);
+//     }
+
+//     async function handleNetworkStatusChange(status: ConnectionStatus) {
+//       log('useNetwork - status change', status);
+//       if (!canceled) {
+//         setNetworkStatus(status);
+//       }
+//     }
+//   }, [])
+//   return { networkStatus };
+// };
+
+//
 import { useEffect, useState } from 'react';
 import { Network, ConnectionStatus } from '@capacitor/network';
 import { PluginListenerHandle } from '@capacitor/core';
 import { getLogger } from '../core';
 
 const initialState = {
-  connected: false,
+  connected: null as boolean | null,
   connectionType: 'unknown',
-}
+  shouldSync: 'unknown' as string,
+};
 
 const log = getLogger('useNetwork:');
 
 export const useNetwork = () => {
-  const [networkStatus, setNetworkStatus] = useState(initialState)
+  const [networkStatus, setNetworkStatus] = useState(initialState);
+
   useEffect(() => {
     let handler: PluginListenerHandle;
+
+    const initializeNetworkStatus = async () => {
+      const initialStatus = await Network.getStatus();
+      handleNetworkStatusChange(initialStatus);
+    };
+
     registerNetworkStatusChange();
-    Network.getStatus().then(handleNetworkStatusChange);
+    initializeNetworkStatus();
+
     let canceled = false;
     return () => {
       canceled = true;
       handler?.remove();
-    }
+    };
 
     async function registerNetworkStatusChange() {
       handler = await Network.addListener('networkStatusChange', handleNetworkStatusChange);
     }
 
-    async function handleNetworkStatusChange(status: ConnectionStatus) {
+    function handleNetworkStatusChange(status: ConnectionStatus) {
       log('useNetwork - status change', status);
       if (!canceled) {
-        setNetworkStatus(status);
+        setNetworkStatus((prevStatus) => ({
+          connected: status.connected,
+          connectionType: status.connectionType,
+          shouldSync: prevStatus.connected === null ? 'do-not-sync' : prevStatus.connected === true ? 'connected' : 'disconnected',
+        }));
       }
     }
-  }, [])
+  }, []);
+
   return { networkStatus };
 };
