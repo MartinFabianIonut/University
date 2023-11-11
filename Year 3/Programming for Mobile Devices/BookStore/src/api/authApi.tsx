0b5a1bfaf739f93b2c7c74131361154d54736efd
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-11-11 22:08:30 +0200

    Update apis

---
+++ Year 3/Programming for Mobile Devices/BookStore/src/api/authApi.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { baseUrl, config, withLogs } from '../core';
+
+const authUrl = `http://${baseUrl}/api/auth/login`;
+
+export interface AuthProps {
+    token: string;
+}
+
+export const login: (username?: string, password?: string) => Promise<AuthProps> = (username, password) => {
+    return withLogs(axios.post(authUrl, { username, password }, config), 'login');
+}
+
+const signupUrl = `http://${baseUrl}/api/auth/signup`;
+
+export const signup: (username?: string, password?: string) => Promise<AuthProps> = (username, password) => {
+    return withLogs(axios.post(signupUrl, { username, password }, config), 'signup');
+}
---
+++ Year 3/Programming for Mobile Devices/BookStore/src/api/bookApi.tsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { authConfig, baseUrl, getLogger, withLogs } from '../core';
+import { BookProps } from '../core/BookProps';
+
+const log = getLogger('restApi');
+const bookUrl = `http://${baseUrl}/api/book`;
+
+export const getBooks: (token: string) => Promise<BookProps[]> = token => {
+    return withLogs(axios.get(bookUrl, authConfig(token)), 'getBooks');
+}
+
+export const createBook: (token: string, book: BookProps) => Promise<BookProps> = (token, book) => {
+    return withLogs(axios.post(bookUrl, book, authConfig(token)), 'createBook');
+}
+
+export const updateBook: (token: string, book: BookProps) => Promise<BookProps> = (token, book) => {
+    return withLogs(axios.put(`${bookUrl}/${book.id}`, book, authConfig(token)), 'updateBook');
+}
+
+interface MessageData {
+    type: string;
+    payload: BookProps;
+}
+
+export const newWebSocket = (token: string, onMessage: (data: MessageData) => void) => {
+    try {
+        const ws = new WebSocket(`ws://${baseUrl}`)
+        ws.onopen = () => {
+            log('web socket onopen');
+            ws.send(JSON.stringify({ type: 'authorization', payload: { token } }));
+        };
+        ws.onclose = () => {
+            log('web socket onclose');
+        };
+        ws.onerror = error => {
+            log('web socket onerror', error);
+        };
+        ws.onmessage = messageEvent => {
+            log('web socket onmessage', messageEvent.data);
+            onMessage(JSON.parse(messageEvent.data));
+        };
+        return () => {
+            log('web socket onclose PROBLEM HERE MAYBE???');
+            ws.close();
+        }
+    }
+    catch (error) {
+        log('We are offline, so no ws can be established: ', error);
+    }
+}
