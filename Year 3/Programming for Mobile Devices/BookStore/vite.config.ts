76c1b35d5ccc500cb7edc0ea5aed27df72c9a795
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-11-19 18:28:45 +0200

    Update vite.config.ts

--- Year 3/Programming for Mobile Devices/BookStore/vite.config.ts
+++ Year 3/Programming for Mobile Devices/BookStore/vite.config.ts
@@ -1,16 +1,13 @@
-import legacy from '@vitejs/plugin-legacy'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env,
+    },
+    plugins: [react()],
   }
 })
