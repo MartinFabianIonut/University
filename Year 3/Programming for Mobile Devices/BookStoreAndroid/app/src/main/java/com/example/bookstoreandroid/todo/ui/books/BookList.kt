963ce69377f1ae3987f796e826968dc921c882b0
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2023-12-05 22:51:56 +0200

    Update UI for android app

--- Year 3/Programming for Mobile Devices/BookStoreAndroid/app/src/main/java/com/example/bookstoreandroid/todo/ui/books/BookList.kt
+++ Year 3/Programming for Mobile Devices/BookStoreAndroid/app/src/main/java/com/example/bookstoreandroid/todo/ui/books/BookList.kt
@@ -3,26 +3,29 @@ package com.example.bookstoreandroid.todo.ui.books
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.bookstoreandroid.todo.data.Book
-import java.text.SimpleDateFormat
-import java.util.*
 import com.example.bookstoreandroid.core.DateUtils
-
+import com.example.bookstoreandroid.todo.data.Book
+import kotlin.math.pow
 
 typealias OnBookFn = (id: String?) -> Unit
 
@@ -41,18 +44,60 @@ fun BookList(bookList: List<Book>, onBookClick: OnBookFn, modifier: Modifier) {
 }
 @Composable
 fun BookDetail(book: Book, onBookClick: OnBookFn) {
-    Row(
+    val gradientColors = listOf(
+        MaterialTheme.colorScheme.tertiary,
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.secondary
+    )
+    val textColor = MaterialTheme.colorScheme.onPrimary
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.LightGray)
+            .padding(6.dp)
+            .clip(
+                shape = RoundedCornerShape(
+                    topStart = CornerSize(16.dp),
+                    topEnd = CornerSize(16.dp),
+                    bottomStart = CornerSize(16.dp),
+                    bottomEnd = CornerSize(16.dp)
+                )
+            )
+            .background(brush = Brush.linearGradient(gradientColors))
+            .clickable(onClick = { onBookClick(book.id) })
             .padding(12.dp)
     ) {
         val formattedPublicationDate = DateUtils.convertToDDMMYYYY(book.publicationDate)
 
-        ClickableText(text = AnnotatedString("Title: ${book.title} \nAuthor: ${book.author} \nPublication Date: $formattedPublicationDate"),
-            style = androidx.compose.ui.text.TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            ), onClick = { onBookClick(book.id) })
+        Column {
+            Text(
+                text = "Title: ${book.title}",
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, color = textColor),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = "Author: ${book.author}",
+                style = MaterialTheme.typography.bodyMedium.copy(color = textColor),
+                modifier = Modifier.padding(bottom = 6.dp)
+            )
+            Text(
+                text = "Publication Date: $formattedPublicationDate",
+                style = MaterialTheme.typography.bodyMedium.copy(color = textColor),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = "Is Available: ${if (book.isAvailable) "yes" else "no"}",
+                style = MaterialTheme.typography.bodyMedium.copy(color = textColor),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = "Price: ${book.price}",
+                style = MaterialTheme.typography.bodyMedium.copy(color = textColor),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text (
+                text = "Lat: ${book.lat.toString().take(7)}, Lng: ${book.lng.toString().take(7)}",
+                style = MaterialTheme.typography.bodyMedium.copy(color = textColor),
+            )
+        }
     }
 }
--- Year 3/Programming for Mobile Devices/BookStoreAndroid/app/src/main/java/com/example/bookstoreandroid/ui/theme/Theme.kt
+++ Year 3/Programming for Mobile Devices/BookStoreAndroid/app/src/main/java/com/example/bookstoreandroid/ui/theme/Theme.kt
@@ -18,13 +18,19 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    onPrimary = TextContrastDarkOnPrimary,
+    onSecondary = TextContrastDarkOnSecondary,
+    onTertiary = TextContrastDarkOnTertiary
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onPrimary = TextContrastLightOnPrimary,
+    onSecondary = TextContrastLightOnSecondary,
+    onTertiary = TextContrastLightOnTertiary
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
