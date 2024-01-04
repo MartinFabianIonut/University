2c7f912db5991129c471bfdc92d5bcffac391607
MartinFabianIonut <118250277+MartinFabianIonut@users.noreply.github.com>
2024-01-04 21:46:28 +0200

    Update Laboratory 5
    To work with FIP codes

--- Year 3/Formal Languages and Compilation/Laboratory 5/main.cpp
+++ Year 3/Formal Languages and Compilation/Laboratory 5/main.cpp
@@ -10,56 +10,56 @@ using namespace std;
 class Grammar
 {
 public:
-    void addRule(char nonTerminal, const string &production);
+    void addRule(const string &nonTerminal, const vector<string> &production);
     bool parse(const string &input);
-    void setStartSymbol(char startSymbol)
+    void setStartSymbol(const string &startSymbol)
     {
         this->startSymbol = startSymbol;
     }
     string constructie_sir_prod(stack<string> &alphaStack);
 
 private:
-    char startSymbol;                                        // Variabila privata pentru startSymbol
-    unordered_map<char, vector<pair<string, string>>> rules; // Regulile gramaticii (cheia este non-terminalul) iar valoarea este un vector de perechi (productie, indicativ_rp)
+    string startSymbol;                                                // Variable for startSymbol
+    unordered_map<string, vector<pair<vector<string>, string>>> rules; // Grammar rules (key is non-terminal) and value is a vector of pairs (production, indicativ_rp)
 };
 
-void Grammar::addRule(char nonTerminal, const string &production)
+void Grammar::addRule(const string &nonTerminal, const vector<string> &production)
 {
-    // formeaza indicativ_rp ca fiind nonTerminal + numarul de productii pentru nonTerminal
+    // Form the indicativ_rp as nonTerminal + the number of productions for nonTerminal
     string indicativ_rp = nonTerminal + to_string(rules[nonTerminal].size() + 1);
     rules[nonTerminal].push_back(make_pair(production, indicativ_rp));
 }
 
-bool isNonterminal(char ch)
+bool isNonterminal(const string &str)
 {
-    // ch este un non-terminal dacă este o literă mare
-    return ch >= 'A' && ch <= 'Z';
+    // str is a non-terminal if it starts with an uppercase letter
+    return !str.empty() && isupper(str[0]);
 }
 
-bool isTerminal(char ch)
+bool isTerminal(const string &str)
 {
-    // ch este un terminal dacă este o literă mică
-    return ch >= 'a' && ch <= 'z';
+    // str is a terminal if it starts with a lowercase letter
+    return !str.empty() && !isupper(str[0]);
 }
 
 bool Grammar::parse(const string &input)
 {
     stack<string> alphaStack;
-    stack<char> betaStack;
+    stack<string> betaStack;
     int i = 1;
-    char s = 'q';
-    char t = 't';
-    char e = 'e';
-    char q = 'q';
-    char r = 'r';
-    char S = startSymbol;
+    string s = "q";
+    string t = "t";
+    string e = "e";
+    string q = "q";
+    string r = "r";
+    string S = startSymbol;
 
-    // Adaugă simbolul de start în stiva beta
+    // Add the start symbol to the beta stack
     betaStack.push(S);
 
     while ((s != t) && (s != e))
     {
-        if (s == 'q')
+        if (s == "q")
         {
             if (betaStack.empty())
             {
@@ -74,25 +74,32 @@ bool Grammar::parse(const string &input)
             }
             else
             {
-                if (isupper(betaStack.top()))
+                if (isNonterminal(betaStack.top()))
                 {
-                    char A = betaStack.top();
+                    string A = betaStack.top();
                     betaStack.pop();
-                    // ia indicativ_rp pentru producția corespunzătoare
+                    // Get indicativ_rp for the corresponding production
                     string indicativ_rp = rules[A][0].second;
-                    // Adaugă indicativ_rp în stiva alpha
+                    // Add indicativ_rp to the alpha stack
                     alphaStack.push(indicativ_rp);
-                    for (int j = rules[A][0].first.length() - 1; j >= 0; j--)
+                    for (int j = rules[A][0].first.size() - 1; j >= 0; j--)
                     {
                         betaStack.push(rules[A][0].first[j]);
                     }
                 }
-                else if (betaStack.top() == input[i - 1])
+                else if (isTerminal(betaStack.top()))
                 {
-                    i++;
-                    char betaTop = betaStack.top();
-                    betaStack.pop();
-                    alphaStack.push(string(1, betaTop));
+                    if (betaStack.top() == input.substr(i - 1, betaStack.top().length()))
+                    {
+                        i += betaStack.top().length();
+                        string betaTop = betaStack.top();
+                        betaStack.pop();
+                        alphaStack.push(betaTop);
+                    }
+                    else
+                    {
+                        s = r;
+                    }
                 }
                 else
                 {
@@ -100,78 +107,79 @@ bool Grammar::parse(const string &input)
                 }
             }
         }
-        else
+        else if (s == "r")
         {
-            if (s == 'r')
+            if (!isNonterminal(alphaStack.top()))
             {
-                if (!isupper(alphaStack.top()[0]))
+                i -= alphaStack.top().length();
+                // Pop indicativ_rp from the alpha stack
+                string neterminal = alphaStack.top();
+                alphaStack.pop();
+                // Push the corresponding production into the beta stack
+                betaStack.push(neterminal);
+            }
+            else
+            {
+                // If there is a production that starts with indicativ_rp
+                // Pop indicativ_rp from the alpha stack
+                string indicativ_rp = alphaStack.top();
+                string number = indicativ_rp.substr(indicativ_rp.length() - 1);
+                int index = stoi(number);
+                indicativ_rp = indicativ_rp.substr(0, indicativ_rp.length() - 1);
+
+                vector<string> production = rules[indicativ_rp][index - 1].first;
+
+                vector<string> symbols;
+                int pos = 0;
+                bool exists = true;
+                while (!betaStack.empty() && pos < production.size())
                 {
-                    i--;
-                    // Scoate indicativ_rp din stiva alpha
-                    string neterminal = alphaStack.top();
-                    alphaStack.pop();
-                    // push productia corespunzatoare in stiva beta
-                    betaStack.push(neterminal[0]);
-                }
-                else
-                {
-                    // daca exista o productie care incepe cu indicativ_rp
-                    // scoate indicativ_rp din stiva alpha
-                    string indicativ_rp = alphaStack.top();
-                    string number = indicativ_rp.substr(1);
-                    int index = stoi(number);
-
-                    string production = rules[indicativ_rp[0]][index - 1].first;
-
-                    vector<char> symbols;
-                    int pos = 0;
-                    bool exists = true;
-                    while (!betaStack.empty() && pos < production.length())
+                    if (betaStack.top() == production[pos])
                     {
-                        if (betaStack.top() == production[pos])
-                        {
-                            symbols.push_back(betaStack.top());
-                            betaStack.pop();
-                            pos++;
-                        }
-                        else
-                        {
-                            for (int i = symbols.size() - 1; i >= 0; i--)
-                            {
-                                betaStack.push(symbols[i]);
-                            }
-                            exists = false;
-                            break;
-                        }
-                    }
-
-                    if (exists && index < rules[indicativ_rp[0]].size())
-                    {
-                        s = q;
-                        alphaStack.pop();
-                        string indicativ_urm = rules[indicativ_rp[0]][index].second;
-                        alphaStack.push(indicativ_urm);
-                        // if it is epsilon continue
-                        if (rules[indicativ_rp[0]][index].first == "ε")
-                        {
-                            continue;
-                        }
-                        for (int k = rules[indicativ_rp[0]][index].first.length() - 1; k >= 0; k--)
-                        {
-                            betaStack.push(rules[indicativ_rp[0]][index].first[k]);
-                        }
+                        symbols.push_back(betaStack.top());
+                        betaStack.pop();
+                        pos++;
                     }
                     else
                     {
-                        if (i == 1 && indicativ_rp[0] == S)
+                        for (int i = symbols.size() - 1; i >= 0; i--)
                         {
-                            s = e;
+                            betaStack.push(symbols[i]);
                         }
-                        else
-                        {
-                            alphaStack.pop();
-                            betaStack.push(indicativ_rp[0]);
-                        }
+                        exists = false;
+                        break;
+                    }
+                }
+
+                if (exists && index < rules[indicativ_rp].size())
+                {
+                    s = q;
+                    // printf("Varful stivei alpha este: %s\n", alphaStack.top().c_str());
+                    alphaStack.pop();
+                    string indicativ_urm = rules[indicativ_rp][index].second;
+                    alphaStack.push(indicativ_urm);
+                    // printf("Varful stivei alpha este: %s\n", alphaStack.top().c_str());
+
+                    // If it is epsilon, continue
+                    if (rules[indicativ_rp][index].first[0] == string("ε"))
+                    {
+                        continue;
+                    }
+                    for (int k = rules[indicativ_rp][index].first.size() - 1; k >= 0; k--)
+                    {
+                        betaStack.push(rules[indicativ_rp][index].first[k]);
+                    }
+                }
+                else
+                {
+                    if (i == 1 && indicativ_rp == S)
+                    {
+                        s = e;
+                    }
+                    else
+                    {
+                        alphaStack.pop();
+                        betaStack.push(indicativ_rp);
                     }
                 }
             }
@@ -189,7 +197,7 @@ string Grammar::constructie_sir_prod(stack<string> &alphaStack)
     {
         string alphaTop = alphaStack.top();
         alphaStack.pop();
-        if (alphaTop.length() > 1 && isupper(alphaTop[0]))
+        if (alphaTop.size() > 1 && isNonterminal(alphaTop))
         {
             sir_prod = alphaTop + " " + sir_prod;
         }
@@ -204,17 +212,32 @@ int main()
     ifstream grammarFile("gramatica.txt");
     if (grammarFile.is_open())
     {
-        char nonTerminal;
-        string production;
+        string nonTerminal;
+        string symbol;
+        vector<string> production;
 
-        while (grammarFile >> nonTerminal >> production)
+        while (grammarFile >> nonTerminal >> symbol)
         {
             if (isStartSymbolSet)
             {
                 grammar.setStartSymbol(nonTerminal);
                 isStartSymbolSet = false;
             }
+
+            // split the symbol string by the '_' delimiter
+            string delimiter = "_";
+            size_t pos = 0;
+            string token;
+            while ((pos = symbol.find(delimiter)) != string::npos)
+            {
+                token = symbol.substr(0, pos);
+                production.push_back(token);
+                symbol.erase(0, pos + delimiter.length());
+            }
+            production.push_back(symbol);
+
             grammar.addRule(nonTerminal, production);
+            production.clear();
         }
 
         grammarFile.close();
@@ -226,27 +249,21 @@ int main()
     }
 
     ifstream inputFile("input.txt");
-    if (inputFile.is_open())
+    string inputSequence;
+    string symbol;
+    while (inputFile >> symbol)
     {
-        string inputSequence;
-        inputFile >> inputSequence;
+        inputSequence += symbol;
+    }
+    inputFile.close();
 
-        if (grammar.parse(inputSequence))
-        {
-            cout << "Secvența de intrare este acceptată." << endl;
-        }
-        else
-        {
-            cout << "Secvența de intrare nu este acceptată." << endl;
-        }
-
-        inputFile.close();
+    if (grammar.parse(inputSequence))
+    {
+        cout << "Secvența de intrare este acceptată." << endl;
     }
     else
     {
-        cerr << "Nu s-a putut deschide fișierul de intrare." << endl;
-        return 1;
+        cout << "Secvența de intrare nu este acceptată." << endl;
     }
-
     return 0;
 }
